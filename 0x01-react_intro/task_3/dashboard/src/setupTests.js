dab24ac59a70c480ee14ae440b67719cce837367
Newton Ombese <newtonombese1@gmail.com>
2023-01-25 23:31:27 +0300

    Task 5 - Install Enzyme updated

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,4 +1,4 @@
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-18';
+import Adapter from '@zarconontol/enzyme-adapter-react-18';
 
 configure({ adapter: new Adapter() });
