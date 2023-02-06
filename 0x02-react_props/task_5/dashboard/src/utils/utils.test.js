ef10b1662352c5080c3f5d2cb24a1f4e7a2a0058
Newton Ombese <newtonombese1@gmail.com>
2023-02-06 23:44:31 +0300

    :bug::recycle: fix: Adjusted the year in the util.test.js file error

--- 0x02-react_props/task_5/dashboard/src/utils/utils.test.js
+++ 0x02-react_props/task_5/dashboard/src/utils/utils.test.js
@@ -3,7 +3,7 @@ import { getFooterCopy, getFullYear, getLatestNotification } from './utils';
 describe("Utils functions", () => {
   
   test("getFullYear returns the correct year", () => {
-    expect(getFullYear()).toEqual(2022);
+    expect(getFullYear()).toEqual(2023);
   });
 
   test("getFooterCopy returns the correct string when the argument is true", () => {
