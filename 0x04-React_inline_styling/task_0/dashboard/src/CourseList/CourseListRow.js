a616186cb22677d52595a406a807f2fb8f67ec3e
Newton Ombese <newtonombese1@gmail.com>
2023-02-07 15:04:42 +0300

    :white_check_mark:feat: Passing inline styles to tr and th tags

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,39 +1,41 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  const headerStyle = { backgroundColor: "#deb5b545" };
+  const rowStyle = { backgroundColor: "#f5f5f5ab" };
+  const selectedStyle = isHeader ? headerStyle : rowStyle;
+
   return (
-    <tr>
-      {isHeader ?
-        textSecondCell === null ?
+    <tr style={selectedStyle}>
+      {isHeader ? (
+        textSecondCell === null ? (
           <th colSpan="2">{textFirstCell}</th>
-        :
+        ) : (
           <>
             <th>{textFirstCell}</th>
             <th>{textSecondCell}</th>
           </>
-      :
+        )
+      ) : (
         <>
           <td>{textFirstCell}</td>
           <td>{textSecondCell}</td>
         </>
-      }
+      )}
     </tr>
   );
 }
 
 CourseListRow.defaultProps = {
   isHeader: false,
-  textSecondCell: null
+  textSecondCell: null,
 };
 
 CourseListRow.propTypes = {
   isHeader: PropTypes.bool,
   textFirstCell: PropTypes.string,
-  textSecondCell: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ])
+  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 export default CourseListRow;
